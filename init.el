7896ad9ef01c09b3c4f59beffcb47f542a72c2f3
johnnyarojas <johnny.rojas.me@gmail.com>
2026-04-21 19:35:41 -0400

    Updated Config

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-;; init.el — bootstrap only, all real config lives in config.org
+;; CRITICAL: Load dev org-mode BEFORE org-babel-load-file
+(add-to-list 'load-path (expand-file-name "elpa/org/lisp/" user-emacs-directory))
+(require 'org)
 
+;; Now load the literate config
 (org-babel-load-file
  (expand-file-name "config.org" user-emacs-directory))
